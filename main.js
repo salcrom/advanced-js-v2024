2ea138acd75b237f1f413889a280061089dec38b
salcrom <seya.28@gmail.com>
2024-09-13 12:37:10 +0200

    05-async finalizado

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { environmentsComponent } from "./src/concepts/01-environments";
 import { callbacksComponent } from "./src/concepts/02-callbacks";
 import { promiseComponent } from "./src/concepts/03-promises";
 import { promiseRaceComponent } from "./src/concepts/04-promise-race";
+import { asyncComponent } from "./src/concepts/05-async";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -22,4 +23,4 @@ document.querySelector("#app").innerHTML = `
 `;
 
 const element = document.querySelector(".card");
-promiseRaceComponent(element);
+asyncComponent(element);
